6e41d4093441a709f297c752d03de77606df822d
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2021-04-17 22:30:39 -0400

    NavBar

--- irondonkey/src/App.js
+++ irondonkey/src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {Navbar,Nav} from 'react-bootstrap'
+import React, { Component } from 'react';
+class App extends React.Component{
 
-function App() {
+  constructor(props) {
+          super(props);
+          this.state = {
+              loaded: false,
+              regions: 'All Region',
+
+          };
+
+
+      }
+      componentDidMount = async () => {
+          this.setState({ loaded: true })
+      }
+
+
+  
+  render(){
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#aboutus" className="mr-sm-2">About Us</Nav.Link>
+          <Nav.Link href="#contact" className="mr-sm-2">Contact</Nav.Link>
+        </Nav>
+        <Nav className="mr-sm-2">
+        <Nav.Link href="#order" id="order" className="ml-auto ">Order Now</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+  </Navbar>
+    
+    
+    );
+
+  }
 }
 
 export default App;
